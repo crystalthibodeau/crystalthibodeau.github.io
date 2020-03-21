51aefd9d889a76d288b464ef71fe2d8eadaba14b
Crystal Thibodeau <crystalthibodeau2@gmail.com>
2020-03-21 15:41:16 -0500

    js

---
+++ js/keys.js
@@ -0,0 +1,4 @@
+var mapboxToken = "pk.eyJ1IjoiY3J5c3RhbC10aGlib2RlYXUiLCJhIjoiY2s0OThvdmhyMDJ3MDNvcm01a2toYXduNiJ9.6fBxi0L9ISvekf0_pgP0sw";
+var darkSkyKey = "ac0657a1f6150fc148099fb06e9582aa";
+var gitHubKey = " af16179c7a7369dddc128912cf903a4cf0c6e14c";
+
